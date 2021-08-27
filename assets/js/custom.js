db0c174dee9c12d891d719b1f3518b1c3e9afe73
tsh <dominik.matasovsky@brainit.sk>
2021-08-27 12:05:02 +0200

    much better

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,7 +22,7 @@ wow.init();
 jQuery(document).ready(function ($) {
 	// browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is#505050uced
+		//browser window scroll (in pixels) after which the "back to top" link opacity is#808080uced
 		offset_opacity = 1200,
 		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
